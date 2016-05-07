9eded0ed52ca7539f7a0060c28b782f9a6e3f2be
Koichiro <KoichiroWatanabe@outlook.jp>
2016-05-07 15:39:59 +0900

    過去のグラフを取得できるようにしました

--- MessageDatabase.py
+++ MessageDatabase.py
@@ -20,11 +20,40 @@ class MessageDatabase:
 
 	def GetDatePastTime(self, message):
 		if self.__InName(message, ["昨日"]):
-			return [date.today() - datetime.timedelta(days=1)]
+			return date.today() - datetime.timedelta(days=1)
 		if self.__InName(message, ["一昨日"]):
-			return [date.today() - datetime.timedelta(days=2)]
-		return None
+			return date.today() - datetime.timedelta(days=2)
 		
+		datematch = re.search('\d+日前', message)
+		if datematch is not None:
+			days = re.search('\d+', datematch.group())
+			return date.today() - datetime.timedelta(days=int(days.group()))
+
+		datematch = re.search('\d+/\d+/\d+', message)
+		if datematch is not None:
+			return datetime.datetime.strptime(datematch.group(), '%Y/%m/%d')
+
+		year = date.today().year
+		month = date.today().month
+		day = date.today().day
+
+		datematch = re.search('\d+年', message)
+		if datematch is not None:
+			year = int(re.search('\d+', datematch.group()).group())
+
+		datematch = re.search('\d+月', message)
+		if datematch is not None:
+			month = int(re.search('\d+', datematch.group()).group())
+
+		datematch = re.search('\d+日', message)
+		if datematch is not None:
+			day = int(re.search('\d+', datematch.group()).group())
+
+		if date.today() == datetime.date(year, month, day):
+			return None
+
+		return datetime.date(year, month, day)
+
 	def GetNowTime(self, message):
 		if self.__InName(message, ["今の"]):
 			return datetime.datetime.now()
--- messages/msg_temp_graph.py
+++ messages/msg_temp_graph.py
@@ -1,3 +1,41 @@
+import re
+from tweet import *
+from datagetter import TempGetter
+from tempgraph import *
+import sys
+
+from os.path import dirname
+from os.path import sep
+from os      import pardir
+
+sys.path.append(dirname(__file__) + sep + pardir)
+
+# 指定した日時のグラフを取得します
 class msg_temp_graph:
 	def send(self, db, message, home_path):
-		pass
+		bGraph = False
+
+		if db.InGraphName(message):
+			bGraph = True
+
+		if not bGraph:
+			return
+		pasttime = db.GetDatePastTime(message)
+
+		if pasttime is None:
+			return
+
+		tw = tweet()
+		tw.Create(home_path)
+		
+		TempGet = TempGetter()
+		TempGet.Create(home_path)
+		TempGet.GetTemptureFromDevices()
+		
+		date_msg = pasttime.strftime("%Y/%m/%d %H:%M:%S") + "の"
+
+		if bGraph:
+			notify_msg = date_msg + "温度グラフは、こちらです"
+			tg = tempgraph()
+			tg.Do(home_path, pasttime.year, pasttime.month, pasttime.day, notify_msg)
+
